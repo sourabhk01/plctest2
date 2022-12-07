962b8da11a70173ead2e47dfdbbfda0aa2c02e2d
sourabhk01 <89476634+sourabhk01@users.noreply.github.com>
2022-12-07 18:41:45 -0500

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,118 +1,173 @@
-#Showing all the digits that are there 
+# Showing all the digits that are there
 digi = '0123456789'
 
 
 class Ers:
-    def init(s,error_n, detailed):
-       
-        s.error_n = error_n
-        s.detailed = detailed
-    
-    def as_string(s):
-        result  = f'{s.error_n}: {s.detailed}\n'
-        return result
+	def init(s, error_n, detailed):
+		s.error_n = error_n
+		s.detailed = detailed
+
+	def as_string(s):
+		result = f'{s.error_n}: {s.detailed}\n'
+		return result
+
 
 class wrong(Ers):
-    def init(s, detailed):
-        super().init( 'Incorrect Charcter', detailed)
+	def init(s, detailed):
+		super().init('Incorrect Charcter', detailed)
 
-#Token Types 
 
-Tt_Divide      = 'Divide'
-Tt_LeftParen   = 'LeftParen'
-Tt_RightParen   = 'RightParen'
-Tt_Mod      = 'MOD'
-Tt_Less      = 'Less'
-Tt_Great     = 'Great'
-Tt_LessEqual   = 'LessEqual'
-Tt_GreatEqual    = 'GreatEqual'
-Tt_Equal     = 'Equal'
-Tt_NotEqual   = 'NotEqual'
-Tt_Int		= 'Int'
-Tt_Float    = 'Float'
-Tt_Plus     = 'Plus'
-Tt_Minus    = 'Minus'
-Tt_Multiply      = 'Multiply'
 
-#type is the token type and value is the value under the type identified 
+# type is the token type and value is the value under the type identified
 class Token:
-    def init(s, t, value=None):
-        s.t = t
-        s.value = value
-#method used to make the print statement with type of token and the value it is
-    def repr(s):
-        if s.value: return f'{s.t}:{s.value}'
-        return f'{s.t}'
+	def init(s, t, value=None):
+		s.t = t
+		s.value = value
 
-#lexer class goes through each char to see what token type it is 
+	# method used to make the print statement with type of token and the value it is
+	def repr(s):
+		if s.value: return f'{s.t}:{s.value}'
+		return f'{s.t}'
+
+
+# lexer class goes through each char to see what token type it is
 class Lexical:
-    def init(s, text):
-        s.text = text
-        s.pos = -1
-        s.currentChar = None
-        s.advance()
+	# Token Types
 
-#goes to the next character in the text and if there is not another char then it wont run
-    def advance(s):
-        s.pos +=1
-        s.currentChar = s.text[s.pos] if s.pos < len(s.text)  else None
+	# keywords is a list which stores key-value pairs of in-built keywords and their replacements
+	keywords = { #if, else, print,
+		"for": "FOR",
+		"while": "WHILE",
+		"return": "RETURN",
+		"or": "OR",
+		"none": "NONE",
+		"and": "AND",
+		"not": "NOT",
+		"try": "TRY",
+		"class": "CLASS",
+		"false": "FALSE",
+		"def": "DEF",
+		"if" : "IF",
+		"else" : "ELSE",
+		"print" : "PRINT",
+		 }
 
-#start with an empty list for the tokens 
-    def make_tokens(s):
-        tokens = []
-#while loop that goes through each char and see if it matches an on these char 
-        while s.currentChar != None:
-            if s.currentChar in ' \t':
-                s.advance()
-            elif s.current in digi:
-                tokens.append(s.make_number())
-            elif s.currentChar == '+':
-                tokens.append(Token(Tt_Plus))
-                s.advance()
-            elif s.currentChar == '-':
-                tokens.append(Token(Tt_Minus))
-                s.advance()
-            elif s.currentChar == '*':
-                tokens.append(Token(Tt_Multiply))
-                s.advance()
-            elif s.currentChar == '/':
-                tokens.append(Token(Tt_Divide))
-                s.advance()
-            elif s.currentChar == '(':
-                tokens.append(Token(Tt_LeftParen))
-                s.advance()
-            elif s.currentChar == ')':
-                tokens.append(Token(Tt_RightParen))
-                s.advance()
-            elif s.currentChar == '%':
-                tokens.append(Token(Tt_Mod))
-                s.advance()
-            elif s.currentChar == '<':
-                tokens.append(Token(Tt_Less))
-                s.advance()
-            elif s.currentChar == '>':
-                tokens.append(Token(Tt_Great))
-                s.advance()
-            elif s.currentChar == '<=':
-                tokens.append(Token(Tt_LessEqual))
-                s.advance()
-            elif s.currentChar == '>=':
-                tokens.append(Token(Tt_GreatEqual))
-                s.advance()
-            elif s.currentChar == '=':
-                tokens.append(Token(Tt_Equal))
-                s.advance()
-            elif s.currentChar == '!=':
-                tokens.append(Token(Tt_NotEqual))
-                s.advance()
-            else:
-                pos_start = s.pos.copy()
-                char = s.currentChar
-                s.advance()
-                return [], wrong( char )
+	keywords_keys = keywords.keys()
 
-        return tokens, None
+	tokens = {
+		'=': 'Equal',
+		'+': 'Addition',
+		'-': 'Subtraction',
+		'/': 'Division',
+		'*': 'Multiplication',
+		'<': 'Less than',
+		'>': 'Greater than',
+		'%': 'Modular',
+		'<=': 'Less than/Equal',
+		'>=': 'Greater Than/Equal',
+		'int': 'integer type',
+		'float': 'Floating point',
+		'char': 'Character type',
+		'long': 'long int',
+		'str': 'string',
+		':': 'colon',
+		';': 'semi-colon',
+		'.': 'dot',
+		',': 'comma',
+	}
 
-    def make_number(s):
-        num_str = ''
-        decimal = 0
+	token_keys = tokens.keys()
+
+	def init(s, text):
+		s.text = text
+		s.pos = -1
+		s.currentChar = None
+		s.advance()
+
+	# goes to the next character in the text and if there is not another char then it wont run
+	def advance(s):
+		s.pos += 1
+		s.currentChar = s.text[s.pos] if s.pos < len(s.text) else None
+
+	# function to check lexeme s in a file and return its list
+	def check_file(s, tokens, keywords):
+
+		lexeme_list = []
+		file = open("factorial.py")
+		file_content = file.split("\n")
+		for line in file_content:
+			tokens = line.split(' ')
+			print("Tokens:", tokens)
+			# searches for the information above and names the lexeme
+			for token in tokens:
+				if token in tokens.keys():
+					lexeme_list.append(f"Token: {tokens[token]}")
+				elif token in keywords.keys():
+					lexeme_list.append(f"Keyword: {keywords[token]}")
+		return lexeme_list
+
+
+	# function to generate tokens and their respective values
+	def check_tokens(s):
+		keywords = []
+		tokens = []
+		# while loop that goes through each char and see if it matches an on these char
+		while s.currentChar != None:
+			if s.currentChar in ' \t':
+				s.advance()
+			elif s.current in digi:
+				tokens.append(s.make_number())
+			elif s.currentChar == '+':
+				tokens.append(Token(tokens["+"]))
+				s.advance()
+			elif s.currentChar == '-':
+				tokens.append(Token(tokens["-"]))
+				s.advance()
+			elif s.currentChar == '*':
+				tokens.append(Token(tokens["*"]))
+				s.advance()
+			elif s.currentChar == '/':
+				tokens.append(Token(tokens["/"]))
+				s.advance()
+			elif s.currentChar == '(':
+				tokens.append(Token(tokens["("]))
+				s.advance()
+			elif s.currentChar == ')':
+				tokens.append(Token(tokens[")"]))
+				s.advance()
+			elif s.currentChar == '%':
+				tokens.append(Token(tokens["%"]))
+				s.advance()
+			elif s.currentChar == '<':
+				tokens.append(Token(tokens["<"]))
+				s.advance()
+			elif s.currentChar == '>':
+				tokens.append(Token(tokens[">"]))
+				s.advance()
+			elif s.currentChar == '<=':
+				tokens.append(Token(tokens["<="]))
+				s.advance()
+			elif s.currentChar == '>=':
+				tokens.append(Token(tokens[">="]))
+				s.advance()
+			elif s.currentChar == '=':
+				tokens.append(tokens["="])
+				s.advance()
+			elif s.currentChar == '!=':
+				tokens.append(tokens["!="])
+				s.advance()
+			else:
+				pos_start = s.pos.copy()
+				char = s.currentChar
+				s.advance()
+
+			# Calling check_file() to process all lexemes in a file
+		lexeme_list = s.check_file(tokens, keywords)
+
+		print("Lexeme list in file: ",lexeme_list)
+
+		return tokens, None
+
+	def make_number(s):
+		num_str = ''
+		decimal = 0
